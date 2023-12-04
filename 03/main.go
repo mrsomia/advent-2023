89c9ef3afb8f1da6e1cb8f869a71db9aba96cbba
Sachin Somia <sachinsomia@gmail.com>
2023-12-04 19:26:36 +0000

    feat: Day3 part 2

--- 03/main.go
+++ 03/main.go
@@ -52,6 +52,31 @@ func getNeighbours(chars [][]string, x, y int) []string {
 	return res
 }
 
+type NeighbourWithCoords struct {
+  x int
+  y int
+  value string
+}
+
+func getNeighboursWithCoords(chars [][]string, x, y int) []NeighbourWithCoords {
+	dirs := [][]int{{1, 0}, {1, 1}, {0, 1}, {0, -1}, {-1, -1}, {-1, 0}, {1, -1}, {-1, 1}}
+	res := []NeighbourWithCoords{}
+	for _, dir := range dirs {
+		dx := dir[0]
+		dy := dir[1]
+		newY := y + dy
+		newX := x + dx
+		if newX >= len(chars[y]) || newX < 0 {
+			continue
+		}
+		if newY >= len(chars) || newY < 0 {
+			continue
+		}
+    res = append(res, NeighbourWithCoords{value: chars[newY][newX], x: newX, y: newY })
+	}
+	return res
+}
+
 func isDot(s string) bool {
 	return s == "."
 }
@@ -127,7 +152,107 @@ func part1(s string) {
 	fmt.Println(r)
 }
 
+func isStar (s string) bool {
+  return "*" == s
+}
+
+func part2(s string) {
+	chars := parseLines(s)
+	// fmt.Println(chars)
+
+	stars := map[string][]int{}
+	currentNum := ""
+  nextToStarAt := ""
+
+	for y, row := range chars {
+		for x, char := range row {
+			isNumber := isCharANumber(char)
+			if !isNumber && isDot(char) && nextToStarAt != "" {
+				//add to validnums
+				n, err := strconv.Atoi(currentNum)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+          v, ok := stars[nextToStarAt]
+          if ok {
+            stars[nextToStarAt] = append(v, n)
+          } else {
+            stars[nextToStarAt] = []int{n}
+          }
+				}
+				currentNum = ""
+        nextToStarAt = ""
+			}
+
+			if isDot(char) && nextToStarAt != ""{
+				currentNum = ""
+			}
+
+			if isNumber {
+				currentNum = currentNum + char
+				// Check neighbours for symbol
+				neighbours := getNeighboursWithCoords(chars, x, y)
+				for _, neighbour := range neighbours {
+					if isStar(neighbour.value) {
+						nextToStarAt = fmt.Sprintf("%v,%v", neighbour.x, neighbour.y)
+					}
+				}
+
+			} else if nextToStarAt != "" {
+				// Add to validnums
+				n, err := strconv.Atoi(currentNum)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+          v, ok := stars[nextToStarAt]
+          if ok {
+            stars[nextToStarAt] = append(v, n)
+          } else {
+            stars[nextToStarAt] = []int{n}
+          }
+				}
+				currentNum = ""
+        nextToStarAt = ""
+			}
+
+			if isDot(char) && nextToStarAt == "" && !isNumber {
+				currentNum = ""
+			}
+
+      // fmt.Printf("\nchar: %v\nnextToStarAt: %v\ncurrentNum: %v\nstars: %v\n", char, nextToStarAt, currentNum, stars)
+		}
+		if nextToStarAt != "" {
+				n, err := strconv.Atoi(currentNum)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+          v, ok := stars[nextToStarAt]
+          if ok {
+            stars[nextToStarAt] = append(v, n)
+          } else {
+            stars[nextToStarAt] = []int{n}
+          }
+				}
+		}
+    currentNum = ""
+    nextToStarAt = ""
+
+	}
+	// fmt.Println(stars)
+
+  c := 0
+
+  for _, v := range stars {
+    if len(v) == 2 {
+      c+= (v[0] * v[1])
+    }
+  }
+  fmt.Println(c)
+}
+
+
 func main() {
 	str := openFile("input1.txt")
 	part1(str)
+  part2(str)
 }
